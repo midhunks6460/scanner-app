5de83c54da67596b645e6eb187c2fcd7620d0c7a
Midhun K S <61262745+midhunks6460@users.noreply.github.com>
2023-11-26 21:59:42 +0530

    Add files via upload

---
+++ scanner/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return render_template('index.html', error='No file part')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return render_template('index.html', error='No selected file')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('index.html', success='File uploaded successfully', filename=filename)
+
+    return render_template('index.html', error='Invalid file format')
+
+if __name__ == '__main__':
+    app.run(debug=True)
